48d13d003072cfa9a1e46edb1840f9d89d5a1840
lck <2282893661@qq.com>
2023-07-17 21:17:51 +0800

    spring与junit的整合

---
+++ spring6-junit/src/main/java/com/lvck1/spring6/junit5/User.java
@@ -0,0 +1,10 @@
+package com.lvck1.spring6.junit5;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+    public void run(){
+        System.out.println("User ......");
+    }
+}
---
+++ spring6-junit/src/test/java/SpringTestJunit5.java
@@ -0,0 +1,22 @@
+import com.lvck1.spring6.junit5.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("classpath:bean.xml")
+//@SpringJUnitConfig(locations = "classpath:bean.xml")
+public class SpringTestJunit5 {
+
+    @Autowired
+    private User user;
+
+    @Test
+    public void testUser(){
+        System.out.println(user);
+        user.run();
+    }
+}
