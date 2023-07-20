60cc14180ca23e7e6698fb330441c4b152b01d29
lck <2282893661@qq.com>
2023-07-20 21:35:00 +0800

    jdbc Template

---
+++ spring-jdbc-tx/src/main/java/com/lvck1/spring6/jdbc/Emp.java
@@ -0,0 +1,53 @@
+package com.lvck1.spring6.jdbc;
+
+public class Emp {
+    private int id;
+
+    private String name;
+
+    private int age;
+
+    private String sex;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "Emp{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}
---
+++ spring-jdbc-tx/src/test/java/JdbcTemplateTest.java
@@ -0,0 +1,84 @@
+import com.lvck1.spring6.jdbc.Emp;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import java.util.List;
+
+@SpringJUnitConfig(locations = "classpath:beans.xml")
+public class JdbcTemplateTest {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    //添加，修改，删除操作
+    @Test
+    public void testUpdate() {
+        //添加操作
+        //第一步：编写SQL语句
+        String sql = "insert into t_emp values(null,?,?,?)";
+        //第二步：调用jdbcTemplete方法，传入相关参数
+//        Object[] params = {"东方不败", 20, "未知"};
+//        int rows = jdbcTemplate.update(sql, params);
+
+        int rows = jdbcTemplate.update(sql, "林平之", 20, "未知");
+        System.out.println(rows);
+
+    }
+
+    @Test
+    public void testUpdate2() {
+        //修改操作
+        String sql = "update t_emp set name = ? where id = ?";
+        int rows = jdbcTemplate.update(sql, "林平之666", 3);
+        System.out.println(rows);
+    }
+
+    @Test
+    public void testUpdate3() {
+        //删除操作
+        String sql = "delete from t_emp where id = ?";
+        int rows = jdbcTemplate.update(sql, 3);
+        System.out.println(rows);
+    }
+
+    //查询操作，返回对象
+    @Test
+    public void testSelectObject() {
+        //写法一
+        String sql = "select * from t_emp where id = ?";
+        Emp empResult = jdbcTemplate.queryForObject(sql,
+                (rs, rowNum) -> {
+                    Emp emp = new Emp();
+                    emp.setId(rs.getInt("id"));
+                    emp.setName(rs.getString("name"));
+                    emp.setAge(rs.getInt("age"));
+                    emp.setSex(rs.getString("sex"));
+                    return emp;
+                },
+                1);
+        System.out.println(empResult);
+
+        //写法二
+        empResult = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Emp.class), 1);
+        System.out.println(empResult);
+    }
+
+    //查询操作，返回list集合
+    @Test
+    public void testSelectList() {
+        String sql = "select * from t_emp";
+        List<Emp> list = jdbcTemplate.query(sql,new BeanPropertyRowMapper<>(Emp.class));
+        System.out.println(list);
+    }
+
+    //查询操作，返回单个值
+    @Test
+    public void testSelectValue(){
+        String sql = "select count(*) from t_emp";
+        int count = jdbcTemplate.queryForObject(sql, Integer.class);
+        System.out.println(count);
+    }
+}
